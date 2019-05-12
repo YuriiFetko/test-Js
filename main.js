adad9dd9009cc9d83c3c14f11917e964ed31bd7c
Yurii <fetkoyura@gmail.com>
2019-05-12 21:46:18 +0300

    home work 1

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log("10" == 10);
+var money = +prompt("Ваш бюджет на месяц?");
+var time = prompt("Введите дату в формате YYYY-MM-DD");
+
+var expensesInMonth = prompt("Введите обязательную статью расходов в этом месяце");
+var res = prompt("Во сколько обойдется?");
+
+var appData = {
+    budget: money,
+    timeData: time,
+    expenses: {
+        expensesInMonth: res
+    },
+    optionalExpenses: {
+
+    },
+    income: [],
+    savings : false
+};
+
+alert( money / 30 );
